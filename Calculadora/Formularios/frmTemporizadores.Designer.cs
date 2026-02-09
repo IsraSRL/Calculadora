da2eac93771e6afe61b3b034a09b3a4f029ff4e5
Isra <kevig600@gmail.com>
2026-02-09 15:47:07 -0600

    Se agrego una alarma con un sonido de gallo

--- Calculadora/Formularios/frmTemporizadores.Designer.cs
+++ Calculadora/Formularios/frmTemporizadores.Designer.cs
@@ -31,13 +31,17 @@
             components = new System.ComponentModel.Container();
             lblHora = new Label();
             tmrHora = new System.Windows.Forms.Timer(components);
+            mnsAlarma = new MenuStrip();
+            establecerToolStripMenuItem = new ToolStripMenuItem();
+            alarma1ToolStripMenuItem = new ToolStripMenuItem();
+            mnsAlarma.SuspendLayout();
             SuspendLayout();
             // 
             // lblHora
             // 
             lblHora.AutoSize = true;
             lblHora.Font = new Font("Arial", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblHora.Location = new Point(45, 27);
+            lblHora.Location = new Point(122, 55);
             lblHora.Name = "lblHora";
             lblHora.Size = new Size(286, 55);
             lblHora.TabIndex = 0;
@@ -49,14 +53,41 @@
             tmrHora.Interval = 1000;
             tmrHora.Tick += tmrHora_Tick;
             // 
+            // mnsAlarma
+            // 
+            mnsAlarma.Items.AddRange(new ToolStripItem[] { establecerToolStripMenuItem });
+            mnsAlarma.Location = new Point(0, 0);
+            mnsAlarma.Name = "mnsAlarma";
+            mnsAlarma.Size = new Size(536, 24);
+            mnsAlarma.TabIndex = 1;
+            mnsAlarma.Text = "menuStrip1";
+            // 
+            // establecerToolStripMenuItem
+            // 
+            establecerToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { alarma1ToolStripMenuItem });
+            establecerToolStripMenuItem.Name = "establecerToolStripMenuItem";
+            establecerToolStripMenuItem.Size = new Size(72, 20);
+            establecerToolStripMenuItem.Text = "Establecer";
+            // 
+            // alarma1ToolStripMenuItem
+            // 
+            alarma1ToolStripMenuItem.Name = "alarma1ToolStripMenuItem";
+            alarma1ToolStripMenuItem.Size = new Size(180, 22);
+            alarma1ToolStripMenuItem.Text = "Alarma1";
+            alarma1ToolStripMenuItem.Click += alarma1ToolStripMenuItem_Click;
+            // 
             // frmTemporizadores
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(560, 111);
+            ClientSize = new Size(536, 157);
             Controls.Add(lblHora);
+            Controls.Add(mnsAlarma);
+            MainMenuStrip = mnsAlarma;
             Name = "frmTemporizadores";
             Text = "Reloj";
+            mnsAlarma.ResumeLayout(false);
+            mnsAlarma.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -65,5 +96,8 @@
 
         private Label lblHora;
         private System.Windows.Forms.Timer tmrHora;
+        private MenuStrip mnsAlarma;
+        private ToolStripMenuItem establecerToolStripMenuItem;
+        private ToolStripMenuItem alarma1ToolStripMenuItem;
     }
 }
--- Calculadora/Formularios/frmTemporizadores.cs
+++ Calculadora/Formularios/frmTemporizadores.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;//para usar visualBasic
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Media;
 using System.Text;
 using System.Windows.Forms;
+using CSCore.SoundOut;
 
 namespace Calculadora.Formularios
 {
     public partial class frmTemporizadores : Form
     {
+        string alarma1 = null;
         public frmTemporizadores()
         {
             InitializeComponent();
@@ -18,6 +23,18 @@ namespace Calculadora.Formularios
         private void tmrHora_Tick(object sender, EventArgs e)
         {
             lblHora.Text = DateTime.Now.ToLongTimeString();
+            
+            if(lblHora.Text == alarma1)
+            {
+                SoundPlayer alarma = new SoundPlayer(@"C:\Users\kevig\source\repos\Calculadora\Calculadora\Sonidos\dragon-studio-rooster-call-364474.wav");
+                alarma.Play();
+
+            }
+        }
+
+        private void alarma1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            alarma1 = Interaction.InputBox("Ingrese la hora: ", "Sistema", "00:00:00 x.x");
         }
     }
 }
