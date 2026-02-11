e765804b42512db33ae20c4a1c41002f02e7fe4a
Isra <kevig600@gmail.com>
2026-02-11 15:50:36 -0600

    Se agrego un editor de texto tipo bloc de notas

---
+++ Calculadora/Formularios/frmEditor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Calculadora.Formularios
+{
+    public partial class frmEditor : Form
+    {
+        bool saved = false;
+        string path = "";
+        public frmEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ofdEditor.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(ofdEditor.FileName))
+                {
+                    rtbEditor.Text = File.ReadAllText(ofdEditor.FileName);
+                }
+            }
+        }
+
+        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Guardar();
+        }
+
+        private void Guardar()
+        {
+            if (sfdEditor.ShowDialog() == DialogResult.OK)
+            {
+                path = sfdEditor.FileName;
+                using (StreamWriter archivo = new StreamWriter(path))
+                {
+                    archivo.Write(rtbEditor.Text);
+                }
+            }
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rtbEditor.Clear();
+            path = "";
+            saved = false;
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(saved == false)
+            {
+                Guardar();
+                saved = true;
+            }
+            else
+            {
+                using (StreamWriter archivo = new StreamWriter(path))
+                {
+                    archivo.Write(rtbEditor.Text);
+                }
+            }
+        }
+    }
+}
